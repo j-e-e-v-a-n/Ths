08ec6201936953b3559afe52ce280fe96b34e52b
Jeevan <103121525+j-e-e-v-a-n@users.noreply.github.com>
2025-05-09 15:11:05 +0530

    result new design

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   // Set the date we're counting down ton
-var countDownDate = new Date("May 8, 2024 16:00:00").getTime();
+var countDownDate = new Date("May 9, 2025 15:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
